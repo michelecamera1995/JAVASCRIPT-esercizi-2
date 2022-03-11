fc2d377331b584f285668c6dc872a42991c061f4
michele camera <michy.cam95@gmail.com>
2022-03-11 14:47:27 +0100

    modify snack 2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,4 +1,38 @@
 console.log("JS-OK")
 
-//
+//Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
 
+
+const numberlist = [];
+
+let somma = 0;
+
+let i
+
+for (i = 0; i < 5; i++) {
+
+    let firstnumb
+
+    while (isNaN(firstnumb)) {
+        firstnumb = parseInt(prompt('Insert a number!'));
+    }
+    if (firstnumb) {
+        numberlist.push(firstnumb);
+    }
+
+}
+
+//for (  i = 0; i < numberlist.length; i++) {
+//    const number = parseInt(numberlist[i]);
+//   somma = somma + number;
+//}
+
+while (i < numberlist.length) {
+    const number = parseInt(numberlist[i]);
+    somma = somma + number;
+}
+
+
+
+console.log(numberlist)
+console.log(somma)
